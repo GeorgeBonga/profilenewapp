27eb2ac06bbfd70d2dc03713c9a7df6ebded6e43
George Bonga <bongageorge18@gmail.com>
2022-11-11 02:59:38 -0800

    Firstcommit

---
+++ src/App.js
@@ -0,0 +1,97 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+    //       <nav>
+    //         <ul>
+    //           <li>
+    //             <a>Home</a>
+    //           </li>
+    //           <li>
+    //             <a>Services</a>
+    //           </li>
+    //           <li>
+    //             <a>Contacts</a>
+    //           </li>
+    //           <li>
+    //             <a>Company</a>
+    //           </li>
+    //           <li>
+    //             <a>Blog</a>
+    //           </li>
+    //           <li>
+    //             <a>Hire</a>
+    //             <ul>
+    //               <li><a>Frontend Developers</a></li>
+    //             </ul>
+    //           </li>
+    //         </ul>
+    //       </nav>
+    //       <section class="main-body">
+    //         <div>
+    //         </div>
+    //       </section>
+
+    //       <section class="footer">
+    //         <footer>
+    //           <div>
+
+    //           </div>
+    //         </footer>
+    //       </section>
+
+
+    //   </header>
+    // </div>
+
+    <html>
+      <head>
+        <link rel="stylesheet" href="./main.css" />
+      </head>
+      <body>
+        <nav>
+          <ul>
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Services</a>
+            </li>
+            <li>
+              <a>Contacts</a>
+            </li>
+            <li>
+              <a>Company</a>
+            </li>
+            <li>
+              <a>Blog</a>
+            </li>
+            <li>
+              <a>Hire</a>
+              <ul>
+                <li><a>Frontend Developers</a></li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+        <section class="main-body">
+          <div>
+          </div>
+        </section>
+
+        <section class="footer">
+          <footer>
+            <div>
+
+            </div>
+          </footer>
+        </section>
+      </body>
+    </html>
+  );
+}
+
+export default App;
